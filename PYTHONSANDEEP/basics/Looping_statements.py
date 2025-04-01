4cfe95e24212570e321aea8fbe5f011617bd618e
Dell <Dell@DESKTOP-CESSRMH>
2025-04-01 10:42:42 +0530

    LOOPING STATEMENTS

---
+++ PYTHONSANDEEP/basics/Looping_statements.py
@@ -0,0 +1,32 @@
+'''
+Created on 1 Apr 2025
+
+@author: SANDEEP:
+
+Looping Statements:
+Statements which executes a code multiple times until a condition is satisfied:
+
+Types:
+1. While loop
+    a. intialisation of a variable
+    b. while condition >> while block
+    c. increment/ decrement
+2. for loop
+
+'''
+
+count=0
+
+while count<5:
+ 
+    #print(count)
+    #print("count<5", count<5)
+    print("Hello World!")
+    #count=count+1
+    count+=1 # increment operator: +=; decrement operator: -=
+    
+#print("count:",count)    
+#print("count<5: ",count<5)
+
+
+
