b7006026cbd5f5e1c968e46e52f89c9157da66c1
Dell <Dell@DESKTOP-CESSRMH>
2025-04-01 11:33:32 +0530

    Variables

---
+++ PYTHONSANDEEP/basics/variables.py
@@ -0,0 +1,47 @@
+'''
+Created on 27 Mar 2025
+variables:
+It is a memory space/ container which stores data
+OR
+It is name given to memory location/ container.
+
+Data stored in this container can be changed.
+
+Creation of variables:
+
+1. We need to specify the type of data the variables stores
+2. We need to give name to the variable
+3. Storing data in the variable (initialization)
+
+DataType VariableName = Value
+
+Invalid cases:
+
+1stnum = 23
+26 = roll_on
+
+'''
+age = 10
+print(age)
+
+num1 = 565
+num2 = 98978
+
+print(num1)
+print(num2)
+
+first_num = 23
+second_num = 45
+
+roll_no = 0
+
+num3, num4 = 34, 56
+print(num3)
+
+num5 = num6 = 46
+print(num5)
+print(num6)
+
+print(num5+num6)
+
+
